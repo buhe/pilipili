8321474a9aaeb26151113fcc60642bd2dffb046e
harebu <bugu1986@126.com>
2016-06-06 16:01:52 +0800

    update

--- index.ios.js
+++ index.ios.js
@@ -21,10 +21,64 @@ import Pili, {
     Player
 } from 'react-native-pili';
 
+//<Player
+//    started={this.state.started}
+//    muted={this.state.muted}
+//    source={{
+//                          uri:"rtmp://live.hkstv.hk.lxdns.com/live/hks",
+//                          backgroundPlay: true,
+//                          //timeout: 10 * 1000,
+//                          //live:true,
+//                          //hardCodec:false,
+//                        }}
+//    style={{
+//                          height:200,
+//                          width:200,
+//                          //backgroundColor:'blue',
+//                          //left:0,
+//                          //top:0
+//                        }}
+//    onLoading={()=>this.setState({text: this.state.text + " loading"})}
+//    onPaused={()=>this.setState({text: "pause"})}
+//    onShutdown={()=>this.setState({text: "shutdown"})}
+//    onError={()=>this.setState({text: "error"})}
+//    onPlaying={()=>this.setState({text: this.state.text + " playing"})}
+//    />
+//<TouchableHighlight onPress={this.start.bind(this)}>
+//<Text style={{height:100,width:100}}>{this.state.started ? "Stop" : "Start"}</Text>
+//</TouchableHighlight>
+//
+//<TouchableHighlight onPress={this.mute.bind(this)}>
+//  <Text style={{height:100,width:100}}>{this.state.muted ? "Sound" : "Mute"}</Text>
+//</TouchableHighlight>
+//
+//<Text>{this.state.text}</Text>
+
+const S1 = {
+  id: "buhe",
+  title: "buhe",
+  hub: "pilitest",
+  publishKey: "6eeee8a82246636e",
+  publishSecurity: "static",
+  hosts: {publish: {rtmp: "pili-publish.pilitest.qiniucdn.com"}}
+};
+
+//rtmp://pili-publish.autodefault.qbox.net/pubhub/yihuan?key=123
+
+const S2 = {
+  id: "yihuan",
+  title: "yihuan",
+  hub: "pubhub",
+  publishKey: "123",
+  publishSecurity: "static",
+  hosts: {publish: {rtmp: "pili-publish.autodefault.qbox.net"}}
+};
+
 class pilipili extends Component {
   constructor() {
     super();
     this.state = {
+      stream: S1,
       muted: false,
       started: true,
       text: '...'
@@ -34,38 +88,28 @@ class pilipili extends Component {
   render() {
     return (
         <View style={styles.container}>
-          <Player
-              started={this.state.started}
-              muted={this.state.muted}
-              source={{
-                          uri:"rtmp://live.hkstv.hk.lxdns.com/live/hks",
-                          backgroundPlay: true,
-                          //timeout: 10 * 1000,
-                          //live:true,
-                          //hardCodec:false,
-                        }}
-              style={{
-                          height:200,
-                          width:200,
-                          //backgroundColor:'blue',
-                          //left:0,
-                          //top:0
-                        }}
-              onLoading={()=>this.setState({text: this.state.text + " loading"})}
-              onPaused={()=>this.setState({text: "pause"})}
-              onShutdown={()=>this.setState({text: "shutdown"})}
-              onError={()=>this.setState({text: "error"})}
-              onPlaying={()=>this.setState({text: this.state.text + " playing"})}
-              />
-          <TouchableHighlight onPress={this.start.bind(this)}>
-            <Text style={{height:100,width:100}}>{this.state.started ? "Stop" : "Start"}</Text>
-          </TouchableHighlight>
-
-          <TouchableHighlight onPress={this.mute.bind(this)}>
-            <Text style={{height:100,width:100}}>{this.state.muted ? "Sound" : "Mute"}</Text>
-          </TouchableHighlight>
-
-          <Text>{this.state.text}</Text>
+          <Streaming
+                stream={this.state.stream}
+                style={{
+                                    height:200,
+                                    width:200,
+                                  }}
+                //zoom={this.state.zoom}
+                //focus={this.state.focus}
+                //profile={{
+                //            video:{
+                //              fps:30,
+                //              bps:1000 * 1024,
+                //              maxFrameInterval:48
+                //            },
+                //            audio:{
+                //              rate:44100,
+                //              bitrate:96 * 1024
+                //            },
+                //          }}
+                //started={this.state.started}
+                //onReady={this.onReady.bind(this)}
+                />
         </View>
     );
   }
